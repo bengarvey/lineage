700892d9235384e73dc64c70d97c32795a2d3239
bgarvey <bgarvey@rjmetrics.com>
2017-01-29 20:11:50 -0500

    finally got exits to work

--- js/lineage.js
+++ js/lineage.js
@@ -1,4 +1,8 @@
-function start() {
+function Lineage() {
+
+  function lin() {
+    console.log("Initializing");
+  }
 
   console.time('init');
 
@@ -10,6 +14,12 @@ function start() {
   var canvas = document.querySelector("canvas"),
       context = canvas.getContext("2d")
 
+  var canvas = d3.select('canvas')
+                  .attr('width', width)
+                  .attr('height', height)
+                  .node();
+  var context = canvas.getContext('2d');
+
   var nodes = [],
       links = [],
       data = {},
@@ -20,11 +30,12 @@ function start() {
       .attr("width", width)
       .attr("height", height);
 
-  var startYear = 1850;
+  var startYear = 1800;
   var year = startYear;
-  var speed = 1000;
+  var speed = 100;
+  var yearIncrement = 0;
   var filters = $('#search').val();
-
+  var searchRadius = 40;
   var simulation = null;
   var g = null;
   var users = [];
@@ -41,20 +52,52 @@ function start() {
 
     users = d3.nest()
       .key(function(d) { return d.id; })
-      .entries(data.nodes);
+      .entries(nodes);
 
     data = prepareData(data, filters);
 
-    simulation = getSimulation(nodes, links);
+    //simulation = getSimulation(nodes, links);
+    canvas
+      .on("mousemove", mousemoved)
+      .call(d3.drag()
+        .container(document.querySelector("canvas"))
+          .subject(dragsubject)
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
 
+    simulation = d3.forceSimulation(nodes)
+      .force("charge", d3.forceManyBody(1))
+      .force("centering", d3.forceCenter(0,0))
+      .force("link", d3.forceLink(links))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY())
+      .alphaTarget(1)
+      .on("tick", ticked);
+
+    d3.interval(loop, speed, d3.now());
+
+    function dragsubject() {
+      console.log("started");
+      return simulation.find(d3.event.x - width / 2, d3.event.y - height / 2, searchRadius);
+    }
+
+    function mousemoved() {
+      var a = this.parentNode, m = d3.mouse(this), d = simulation.find(m[0] - width / 2, m[1] - height / 2, searchRadius);
+      if (!d) return a.removeAttribute("href"), a.removeAttribute("title");
+      a.setAttribute("href", "http://bl.ocks.org/" + (d.user ? d.user + "/" : "") + d.id);
+      a.setAttribute("title", d.id + (d.user ? " by " + d.user : "") + (d.description ? "\n" + d.description : ""));
+    }
+    /*
     g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
         link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.5).selectAll(".link"),
         node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
-
+    */
+    node = canvas.selectAll(".node");
+    link = canvas.selectAll(".link");
     restart();
     console.timeEnd('init');
 
-    d3.interval(loop, speed, d3.now());
   }
 
   function loop() {
@@ -78,7 +121,7 @@ function start() {
   }
 
   function advanceYear(year) {
-    return year + 1;
+    return year + yearIncrement;
   }
 
   function addRemoveNode(n) {
@@ -87,7 +130,7 @@ function start() {
       if (nodes.indexOf(n) == -1 && nodeYear <= year) {
         nodes.push(n);
       }
-      else if (nodes.indexOf(n) > -1 && (nodeYear > year)) {
+      else if (nodes.indexOf(n) != -1 && (nodeYear > year)) {
         nodes.splice(nodes.indexOf(n), 1);
       }
     }
@@ -121,6 +164,15 @@ function start() {
   }
 
   function getSimulation(nodes, links) {
+    canvas
+      .on("mousemove", mousemoved)
+      .call(d3.drag()
+        .container(canvas)
+          .subject(dragsubject)
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
     var simulation = d3.forceSimulation(nodes)
       .force("charge", d3.forceManyBody(1))
       .force("centering", d3.forceCenter(0,0))
@@ -130,7 +182,20 @@ function start() {
       .alphaTarget(1)
       .on("tick", ticked);
     return simulation;
+
+  function dragsubject() {
+    console.log("started");
+    return simulation.find(d3.event.x - width / 2, d3.event.y - height / 2, searchRadius);
   }
+
+  function mousemoved() {
+    var a = this.parentNode, m = d3.mouse(this), d = simulation.find(m[0] - width / 2, m[1] - height / 2, searchRadius);
+    if (!d) return a.removeAttribute("href"), a.removeAttribute("title");
+    a.setAttribute("href", "http://bl.ocks.org/" + (d.user ? d.user + "/" : "") + d.id);
+    a.setAttribute("title", d.id + (d.user ? " by " + d.user : "") + (d.description ? "\n" + d.description : ""));
+  }
+}
+
 
   function updateYear(year) {
     $('#year').html(year)
@@ -163,35 +228,25 @@ function start() {
     updateYear(year);
     //updateFilter();
     // Apply the general update pattern to the nodes.
+    /*
     node = node.data(nodes, function(d) { return d.id;});
-    node.exit().remove();
-    node = node.enter()
-      .append("circle")
-      .attr("fill", function(d) { return color(d.lastName); })
-      .attr("r", 5)
-      .attr("x", width/2)
-      .attr("y", height/2)
-      .merge(node)
-      .call(d3.drag()
-        .on("start", function(d) {dragstarted(d, simulation);})
-        .on("drag", dragged)
-        .on("end", function(d) {dragended(d, simulation);}));
-
-    node.append("title")
-      .text(function(d) { return d.name; });
+    node.enter().merge(node);
+    node.exit().attr('opacity', 1).transition().duration(500).attr('opacity', 0).remove();
 
     // Apply the general update pattern to the links.
     link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
-    link.exit().remove();
-    link = link.enter()
-      .append("line")
-      .style("stroke-width", 1)
-      .style("stroke", function(d) { return d.color; })
-      .merge(link);
+    link.enter().merge(link);
+    link.exit().transition().attr("stroke-opacity", 0).remove();
+    */
+
+    users = d3.nest()
+      .key(function(d) { return d.id; })
+      .entries(nodes);
 
     // Update and restart the simulation.
     simulation.nodes(nodes);
     simulation.force("link").links(links);
+    simulation.alpha(1).restart();
   }
 
   function ticked() {
@@ -200,7 +255,7 @@ function start() {
     context.save();
     context.translate(width / 2, height / 2);
 
-    data.links.forEach(drawLink);
+    links.forEach(drawLink);
 
     users.forEach(function(user) {
       context.beginPath();
@@ -236,21 +291,23 @@ function start() {
     return false;
   }
 
-  function dragstarted(d, sim) {
-    if (!d3.event.active) sim.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+
+  function dragstarted() {
+    console.log("started");
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d3.event.subject.fx = d3.event.subject.x;
+    d3.event.subject.fy = d3.event.subject.y;
   }
 
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
+  function dragged() {
+    d3.event.subject.fx = d3.event.x;
+    d3.event.subject.fy = d3.event.y;
   }
 
-  function dragended(d, sim) {
-    if (!d3.event.active) sim.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
+  function dragended() {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d3.event.subject.fx = null;
+    d3.event.subject.fy = null;
   }
 
   function initializeNav() {
@@ -287,4 +344,18 @@ function start() {
     context.moveTo(d.x + 3, d.y);
     context.arc(d.x, d.y, 5, 0, 2 * Math.PI);
   }
+
+  lin.setYear = function(value) {
+    year = value;
+  }
+
+  lin.moveYear = function(value) {
+    year += value
+  }
+
+  lin.setYearIncrement = function(value) {
+    yearIncrement = value;
+  }
+
+  return lin;
 }
