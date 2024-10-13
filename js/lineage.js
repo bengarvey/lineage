e97e5720161e0914376537a4a0eeee84d0cfbf9e
Ben Garvey <ben@bengarvey.com>
2024-10-12 23:01:26 -0400

    Upgrade to d3 v7.9 (#32)

--- js/lineage.js
+++ js/lineage.js
@@ -1,7 +1,7 @@
 function Lineage() {
 
   function lin(conf) {
-    log("Initializing", config);
+    log("Initializing", conf);
     initNightMode();
     initSlider();
     config = conf;
@@ -12,7 +12,7 @@ function Lineage() {
 
   // Will be overwritten by `lin()`
   var config = {
-    startYear:2014,
+    startYear: 2014,
     endYear: 2014,
     speed: 100,
     debug: false
@@ -29,12 +29,12 @@ function Lineage() {
   var TIMELINE_SPEED = 0.8;
 
   var svg = d3.select("svg");
-      width = window.innerWidth,
+  var width = window.innerWidth,
       height = window.innerHeight,
-      color = d3.scaleOrdinal(d3.schemeCategory20);
+      color = d3.scaleOrdinal(d3.schemeCategory10);
 
   var canvas = document.querySelector("canvas"),
-      context = canvas.getContext("2d")
+      context = canvas.getContext("2d");
 
   var canvas = d3.select('canvas')
                   .attr('width', width)
@@ -62,28 +62,23 @@ function Lineage() {
   var g = null;
   var users = [];
   var interval = null;
-  var mode = 'tree'
+  var mode = 'tree';
 
-  // Do i have to re-populate `nodes` and `links` in `loop()`?
   var forceRefresh = true;
-
 
   function initShowDead(value) {
     showDead = value;
     $('#showDead').prop('checked', showDead);
   }
 
-  function go(error, response) {
-    if (error) throw error;
-
+  function go(response) {
     init(response);
     forceRefresh = true;
     if (interval != null) {
       interval.stop();
     }
-    interval = d3.interval(loop, config.speed, d3.now());
+    interval = d3.interval(loop, config.speed);
   }
-
 
   function reinit(response) {
     links = [];
@@ -91,17 +86,13 @@ function Lineage() {
     restart();
   }
 
-
- function init(response) {
+  function init(response) {
     nodes = [];
     links = [];
     originalData = jQuery.extend(true, {}, response);
     data = response;
 
-    users = d3.nest()
-      .key(function(d) { return d.id; })
-      .entries(nodes);
-
+    users = d3.group(nodes, d => d.id);
     data = prepareData(data, filters);
     simulation = d3.forceSimulation(nodes);
     [canvas, simulation] = getCanvasSimulation(mode);
@@ -112,36 +103,32 @@ function Lineage() {
     timeEnd('init', config);
   }
 
-
   function getCanvasSimulation(mode) {
     canvas
       .on("mousemove", mousemoved)
       .call(d3.drag()
         .container(document.querySelector("canvas"))
-          .subject(dragsubject)
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+        .subject(dragsubject)
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
 
     var sim = null;
     if (mode == 'tree') {
       sim = getTreeSimulation();
-    }
-    else if (mode == 'timeline') {
+    } else if (mode == 'timeline') {
       sim = getTimelineSimulation();
-    }
-    else if (mode == 'cluster') {
+    } else if (mode == 'cluster') {
       sim = getClusterSimulation();
     }
 
     return [canvas, sim];
   }
 
-
   function getClusterSimulation() {
     simulation
       .force("charge", d3.forceManyBody().strength(-5))
-      .force("centering", d3.forceCenter(0,0))
+      .force("centering", d3.forceCenter(0, 0))
       .force("link", d3.forceLink([]).strength(-1))
       .force("x", d3.forceX())
       .force("y", d3.forceY())
@@ -151,11 +138,10 @@ function Lineage() {
     return simulation;
   }
 
-
   function getTreeSimulation() {
     simulation
       .force("charge", d3.forceManyBody().strength(-50))
-      .force("centering", d3.forceCenter(0,0))
+      .force("centering", d3.forceCenter(0, 0))
       .force("link", d3.forceLink(links).distance(30).strength(0.5))
       .force("x", d3.forceX())
       .force("y", d3.forceY())
@@ -177,18 +163,18 @@ function Lineage() {
     return simulation;
   }
 
-  function mousemoved() {
-    var a = this.parentNode, m = d3.mouse(this), d = simulation.find(m[0] - width / 2, m[1] - height / 2, searchRadius);
+  function mousemoved(event) {
+    var m = d3.pointer(event, this);
+    var d = simulation.find(m[0] - width / 2, m[1] - height / 2, searchRadius);
     if (!d) {
-      hideMemberDetails(); 
-    }
-    else {
+      hideMemberDetails();
+    } else {
       highlightNode(d, m);
     }
   }
 
   function dragsubject() {
-    return simulation.find(d3.event.x - width / 2, d3.event.y - height / 2, searchRadius);
+    return simulation.find(event.x - width / 2, event.y - height / 2, searchRadius);
   }
 
   function hideMemberDetails() {
@@ -201,9 +187,8 @@ function Lineage() {
       .style('display', 'block')
       .style('top', m[1] - 20)
       .style('left', m[0] + 20);
-    d3.select('#name').html(d.name + "<br><span class='birthYear'>" + d.birthDate.substring(0,4) + "</span>");
+    d3.select('#name').html(d.name + "<br><span class='birthYear'>" + d.birthDate.substring(0, 4) + "</span>");
   }
-
 
   function setForceRefresh(value) {
     forceRefresh = value;
@@ -233,53 +218,28 @@ function Lineage() {
     forceRefresh = false;
   }
 
-
-  /*
-    Only have those people in `nodes` that have been born at the time `year`.
-    To be called via `data.nodes.forEach(addRemoveNodes)` in `loop()`.
-    If `showDead` is true:
-    Only have those people in `nodes` that are currently alive.
-    */
   function addRemoveNode(n) {
-    // Only add people who have been born at this time
     if (n.birthDate != null) {
       var birthYear = n.birthDate.substring(0, 4);
-      if (
-        nodes.indexOf(n) == -1 &&
-        birthYear <= year
-      ) {
+      if (nodes.indexOf(n) == -1 && birthYear <= year) {
         nodes.push(n);
-      }
-      else if (
-        nodes.indexOf(n) != -1 &&
-        (birthYear > year)
-      ) {
+      } else if (nodes.indexOf(n) != -1 && (birthYear > year)) {
         nodes.splice(nodes.indexOf(n), 1);
       }
     }
 
-    // Remove dead people
     if (!showDead) {
       if (n.deathDate != null && n.deathDate != "") {
         var deathYear = Number(n.deathDate.substring(0, 4));
         if (isNaN(deathYear)) return;
 
-        if (
-          nodes.indexOf(n) != -1 &&
-          deathYear < year
-        ) {
+        if (nodes.indexOf(n) != -1 && deathYear < year) {
           nodes.splice(nodes.indexOf(n), 1);
         }
       }
     }
   }
 
-
-  /*
-    Only have links in `link` that specify a connection between people that
-    currently are in `nodes`.
-    To be called via `data.links.forEach(addRemoveLink)` in `loop()`.
-    */
   function addRemoveLink(l) {
     if (
       links.indexOf(l) == -1 &&
@@ -287,8 +247,7 @@ function Lineage() {
       nodes.indexOf(l.target) > -1
     ) {
       links.push(l);
-    }
-    else if (
+    } else if (
       links.indexOf(l) > -1 &&
       (nodes.indexOf(l.source) == -1 || nodes.indexOf(l.target) == -1)
     ) {
@@ -296,38 +255,19 @@ function Lineage() {
     }
   }
 
-
-  /*
-    I think this groups all nodes into clusters based on their last names...
-    Example Output:
-
-    Array []
-      Amidala: Object { x: -999, y: -636 }
-      C3PO: Object { x: -988, y: -431 }
-      Chewbacca: Object { x: -988, y: -759 }
-      Kenobi: Object { x: -988, y: -472 }
-      Organa: Object { x: -999, y: -554 }
-      Palpatine: Object { x: -999, y: -759 }
-      R2D2: Object { x: -988, y: -390 }
-      Skywalker: Object { x: -999, y: -718 }
-      Solo: Object { x: -988, y: -349 }
-      Yoda: Object { x: -999, y: -513 }
-    */
-  // TODO: Add correct Docstring
   function resetClusters(nodes) {
     clusters = [];
-    rowCount = 11;
-    colCount = 13;
-    nodes.forEach( function(n, i) {
-      if(clusters[n.lastName] == null) {
-        var x = Math.round(i / colCount) + Math.round(i/colCount)*CLUSTER_COL_SPACING - width;
-        var y = i % rowCount + Math.round(i%rowCount)*CLUSTER_ROW_SPACING - height;
-        clusters[n.lastName] = {x: x, y: y};
+    var rowCount = 11;
+    var colCount = 13;
+    nodes.forEach(function(n, i) {
+      if (clusters[n.lastName] == null) {
+        var x = Math.round(i / colCount) + Math.round(i / colCount) * CLUSTER_COL_SPACING - width;
+        var y = i % rowCount + Math.round(i % rowCount) * CLUSTER_ROW_SPACING - height;
+        clusters[n.lastName] = { x: x, y: y };
       }
     });
     return clusters;
   }
-
 
   function advanceYear(year) {
     year += yearIncrement;
@@ -337,65 +277,52 @@ function Lineage() {
     return year;
   }
 
-
   function updateFilter() {
     if (filters != $("#search").val()) {
       filters = $("#search").val();
-      go(null, originalData);
+      go(originalData);
     }
   }
 
-
   function updateSlider() {
-    position = ((year - config.startYear) / (config.endYear - config.startYear)) * 100;
+    var position = ((year - config.startYear) / (config.endYear - config.startYear)) * 100;
     $("#yearSlider").val(position);
   }
 
-
-  /*
-    DEPRECATED: No #yearSlider DOM Element
-    */
   function initSlider() {
-    $('#yearSlider').on('change', function(){
-      position = $("#yearSlider").val();
-      year = Math.round(((config.endYear - config.startYear) * (position/100)) + config.startYear);
+    $('#yearSlider').on('change', function() {
+      var position = $("#yearSlider").val();
+      year = Math.round(((config.endYear - config.startYear) * (position / 100)) + config.startYear);
     });
   }
 
-  /*
-    Filter `data.nodes` by `filters`:
-    Only have items in `nodes` For which `inFilter()` returns `true`.
-    */
   function prepareData(data, filters) {
-    filterItems = filters.split(" ");
-    filterItems = filterItems.filter( function(i) {
+
+    var filterItems = filters.split(" ");
+    filterItems = filterItems.filter(function(i) {
       return i.length > 0;
     });
-    for(var i = 0; i < data.nodes.length; i++) {
+    debugger;
+    for (var i = 0; i < data.nodes.length; i++) {
       if (!inFilter(data.nodes[i], filterItems)) {
         data.nodes.splice(i, 1);
         i--;
       }
     }
 
-    // link directly instead of using indices
-    data.links.forEach(function(link, index) {
+    data.links.forEach(function(link) {
       link.source = getNodeById(data.nodes, link.source);
       link.target = getNodeById(data.nodes, link.target);
     });
     return data;
   }
 
-
-  /*
-    Does one of the regex words from the filter occur in this nodes name?
-    */
   function inFilter(node, filterItems) {
     if (filterItems.length == 0) {
       return true;
     }
     var regex = null;
-    for(i=0; i<filterItems.length; i++) {
+    for (var i = 0; i < filterItems.length; i++) {
       regex = new RegExp(filterItems[i], 'ig');
       if (node.name.match(regex)) {
         return true;
@@ -404,13 +331,11 @@ function Lineage() {
     return false;
   }
 
-
   function updateYear(year) {
     $('#year').html(year)
-      .css('left', width/2 - 105)
+      .css('left', width / 2 - 105)
       .css('top', height - 140);
   }
-
 
   function resizeScreen() {
     if (width != window.innerWidth) {
@@ -421,23 +346,18 @@ function Lineage() {
     }
   }
 
-
   function restart() {
     updateYear(year);
-    users = d3.nest()
-      .key(function(d) { return d.id; })
-      .entries(nodes);
+    users = d3.group(nodes, d => d.id);
 
     simulation.nodes(nodes);
     if (mode == 'tree') {
       simulation.force("link").links(links);
-    }
-    else {
+    } else {
       simulation.force("link").links(links).strength(0);
     }
     simulation.alpha(1).restart();
   }
-
 
   function clusterTicked() {
     context.clearRect(0, 0, width, height);
@@ -445,22 +365,21 @@ function Lineage() {
     context.translate(width / 2, height / 2);
 
     var k = 0.1 * simulation.alpha;
-    users.forEach(function(o, i) {
-      u = o.values[0];
+    users.forEach(function(o) {
+      var u = o[0];
       u.y += (clusters[u.lastName].y - u.y) * 0.08;
       u.x += (clusters[u.lastName].x - u.x) * 0.08;
     });
 
     users.forEach(function(user) {
       context.beginPath();
-      user.values.forEach(drawNode);
-      context.fillStyle = color(user.values[0].lastName);
+      user.forEach(drawNode);
+      context.fillStyle = color(user[0].lastName);
       context.fill();
     });
 
     context.restore();
   }
-
 
   function treeTicked() {
     context.clearRect(0, 0, width, height);
@@ -471,67 +390,60 @@ function Lineage() {
 
     users.forEach(function(user) {
       context.beginPath();
-      user.values.forEach(drawNode);
-      context.fillStyle = color(user.values[0].lastName);
+      user.forEach(drawNode);
+      context.fillStyle = color(user[0].lastName);
       context.fill();
     });
 
     context.restore();
   }
 
-
   function timeTicked() {
-
     context.clearRect(0, 0, width, height);
     context.save();
     context.translate(width / 2, height / 2);
 
-    for(i=0; i<users.length; i++) {
-      d = users[i].values[0];
-      scale = ((d.birthDate.substring(0,4) - 1750) / (2020 - 1750) - 0.5);
-      d.x += (width*scale - d.x) * TIMELINE_SPEED;
+    for (var i = 0; i < users.length; i++) {
+      var d = users[i][0];
+      var scale = ((d.birthDate.substring(0, 4) - 1750) / (2020 - 1750) - 0.5);
+      d.x += (width * scale - d.x) * TIMELINE_SPEED;
     }
 
     users.forEach(function(user) {
       context.beginPath();
-      user.values.forEach(drawNode);
-      context.fillStyle = color(user.values[0].lastName);
+      user.forEach(drawNode);
+      context.fillStyle = color(user[0].lastName);
       context.fill();
     });
 
     context.restore();
   }
 
-
-  function dragstarted() {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d3.event.subject.fx = d3.event.subject.x;
-    d3.event.subject.fy = d3.event.subject.y;
+  function dragstarted(event) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
   }
 
-
-  function dragged() {
-    d3.event.subject.fx = d3.event.x;
-    d3.event.subject.fy = d3.event.y;
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
   }
 
-
-  function dragended() {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d3.event.subject.fx = null;
-    d3.event.subject.fy = null;
+  function dragended(event) {
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
   }
-
 
   function getNodeById(nodes, id) {
-    for(i=0; i<nodes.length; i++) {
+    for (var i = 0; i < nodes.length; i++) {
       if (nodes[i].id === id) {
         return nodes[i];
       }
     }
     return -1;
   }
-
 
   function drawLink(d) {
     context.beginPath();
@@ -542,44 +454,39 @@ function Lineage() {
     context.stroke();
   }
 
-
   function drawNode(d) {
     context.moveTo(d.x, d.y);
     context.arc(d.x, d.y, 5, 0, 2 * Math.PI);
   }
 
-
   function initNightMode() {
     $('#nightModeOn').on("change", function(event) {
-        nightMode = !nightMode;
-        body = d3.select('body');
-        main = d3.select('main');
-        menu = d3.select('#menu');
+      nightMode = !nightMode;
+      var body = d3.select('body');
+      var main = d3.select('main');
+      var menu = d3.select('#menu');
 
-        if (nightMode) {
-          body.transition().duration(1000).style('background-color', '#000').style('color', '#EEE');
-          main.transition().duration(1000).style('background-color', '#000').style('color', '#EEE');
-          menu.transition().duration(1000).style('background-color', 'rgba(5, 5, 5, 0.75)').style('color', '#EEE');
-          d3.select('#year').transition().duration(1000).style('color', '#EEE');
-          d3.select('.toggle__button').transition().duration(1000).style('color', '#666').style('background-color', '#333');
-        }
-        else {
-          body.transition().duration(1000).style('background-color', '#FFF').style('color', '#333');
-          main.transition().duration(1000).style('background-color', '#FFF').style('color', '#333');
-          menu.transition().duration(1000).style('background-color', 'rgba(250, 250 , 250, 0.75)').style('color', '#333');
-          d3.select('#year').transition().duration(1000).style('color', '#222');
-          d3.select('.toggle__button').transition().duration(1000).style('color', '#FFF').style('background-color', '#E7E7E7');
-        }
+      if (nightMode) {
+        body.transition().duration(1000).style('background-color', '#000').style('color', '#EEE');
+        main.transition().duration(1000).style('background-color', '#000').style('color', '#EEE');
+        menu.transition().duration(1000).style('background-color', 'rgba(5, 5, 5, 0.75)').style('color', '#EEE');
+        d3.select('#year').transition().duration(1000).style('color', '#EEE');
+        d3.select('.toggle__button').transition().duration(1000).style('color', '#666').style('background-color', '#333');
+      } else {
+        body.transition().duration(1000).style('background-color', '#FFF').style('color', '#333');
+        main.transition().duration(1000).style('background-color', '#FFF').style('color', '#333');
+        menu.transition().duration(1000).style('background-color', 'rgba(250, 250 , 250, 0.75)').style('color', '#333');
+        d3.select('#year').transition().duration(1000).style('color', '#222');
+        d3.select('.toggle__button').transition().duration(1000).style('color', '#FFF').style('background-color', '#E7E7E7');
+      }
     });
   }
 
-
   function timeStart(name, config) {
-    if (config.debug ) {
+    if (config.debug) {
       console.time(name);
     }
   }
-
 
   function timeEnd(name, config) {
     if (config.debug) {
@@ -587,17 +494,15 @@ function Lineage() {
     }
   }
 
-
   function log(message, config) {
     if (config.debug) {
       console.log(message);
     }
   }
 
-
   lin.loadJson = function(path) {
-    d3.json(path, go);
-  }
+    d3.json(path).then(go);
+  };
 
   lin.playMusic = function() {
     if ($('#musicOn').is(":checked")) {
@@ -615,7 +520,7 @@ function Lineage() {
   }
 
   lin.moveYear = function(value) {
-    year += value
+    year += value;
     forceRefresh = true;
   }
 
